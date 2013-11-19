835d440e40a2c6e3afdc66627c34ed98abf6b9a4
James Nzomo <kazikubwa@gmail.com>
2013-11-19 17:24:48 +0300

    End of Part 2 (sans template customizations)

--- polls/models.py
+++ polls/models.py
@@ -2,8 +2,8 @@ import datetime
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __unicode__(self):  # Python 3: def __str__(self):
@@ -12,9 +12,13 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question)
+    poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
